c600a970d64e1b93dac7aff7fe15e76410c2203e
ajmal <ajmal@Populos-Mac-mini-3.local>
2018-08-06 09:32:04 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, isDevMode } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  cssUrl: string;
+
+  constructor(public sanitizer: DomSanitizer) {
+
+  }
+
+  ngOnInit() {
+    const randomIntegerInRange = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+    const n = randomIntegerInRange(1, 100) % 2;
+    this.cssUrl = n % 2 === 0 ? '/assets/styles1.css' : '/assets/styles2.css';
+  }
+
+}
